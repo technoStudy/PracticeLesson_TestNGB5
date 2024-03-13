9c3204ec2fa7c0038fb48a328c5ff52277fa5a16
ChatGTHB <cihat.kose@hotmail.com>
2024-03-13 16:52:21 +0100

    Edited

--- src/_00_TestNGIntro/_04_Assertions.java
+++ src/_00_TestNGIntro/_04_Assertions.java
@@ -15,10 +15,9 @@ public class _04_Assertions {
         Assert.assertEquals(text01, text02);
 
         System.out.println("Test case passed successfully");
-
     }
 
-    @Test (priority = 1)
+    @Test(priority = 1)
     public void testCaseOne() {
         String text01 = "Hello!";
         String text02 = "Hello!";
@@ -27,7 +26,7 @@ public class _04_Assertions {
         System.out.println("Test case one passed successfully");
     }
 
-    @Test (priority = 2)
+    @Test(priority = 2)
     public void testCaseTwo() {
         String text01 = "Java";
         String text02 = "Java";
@@ -36,12 +35,12 @@ public class _04_Assertions {
         System.out.println("Test case two passed successfully");
     }
 
-    @Test (priority = 3)
+    @Test(priority = 3)
     public void testCaseThree() {
         String text01 = "Selenium";
         String text02 = "Selenium";
 
-        Assert.assertSame(text01, text02, "Address of both string objects are not same");
+        Assert.assertSame(text01, text02, "Address of both string objects are not SAME");
         System.out.println("Test case three passed successfully");
     }
 }
